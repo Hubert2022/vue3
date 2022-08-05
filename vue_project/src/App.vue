08aac67e8b32ca842ee983ae7de6355e638f2d77
韩 <278019198@qq.com>
2022-08-05 13:58:32 +0800

    新项目

---
+++ vue_project/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="vu3仿美图"/>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
